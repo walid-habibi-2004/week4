d8dd962d33092ac18b36531f1cd5b532ec6ba75e
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-03-02 21:38:31 +0000

    Update exercice_xp.js

---
+++ day_5/exercices/exercice_xp.js
@@ -0,0 +1,58 @@
+//==================> exercice 1
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/collegeDB')
+  .then(() => {
+    console.log('✅ Connection Successful! Node.js is talking to MongoDB.');
+  })
+  .catch((err) => {
+    console.error('❌ Connection Failed:', err);
+  });
+
+//==================> exercice 1
+
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/collegeDB')
+  .then(() => {
+    console.log('✅ Connection Successful! Node.js is talking to MongoDB.');
+  })
+  .catch((err) => {
+    console.error('❌ Connection Failed:', err);
+  });
+
+const studentSchema = new mongoose.Schema({
+  name: { 
+    type: String, 
+    required: [true, 'Name is mandatory'] 
+  },
+  email: { 
+    type: String, 
+    required: true, 
+    unique: true 
+  },
+  age: { 
+    type: Number, 
+    min: [18, 'Students must be at least 18 years old'] 
+  }
+});
+
+const Student = mongoose.model('Student', studentSchema);
+
+const testStudent = async () => {
+  try {
+    const student = new Student({
+      name: "Omar",
+      email: "omar@school.com",
+      age: 16
+    });
+    await student.save();
+  } catch (error) {
+    console.log("Validation Error Caught:", error.message);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
+testStudent();
