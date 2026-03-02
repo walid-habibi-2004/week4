5be7c0ce0115af9038827a9b808a98df0980c781
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-03-02 21:10:37 +0000

    Update challenge.js

---
+++ day_2/daily_challenge/challenge.js
@@ -0,0 +1,36 @@
+db.users.insertOne({
+  username: "devYasmine",
+  bio: "Full-stack developer passionate about NoSQL.",
+  socialLinks: {
+    github: "github.com/devYasmine",
+    linkedin: "linkedin.com/in/devYasmine",
+    twitter: "twitter.com/devYasmine"
+  }
+})
+
+
+db.posts.insertOne({
+  title: "Getting Started With DevBlog",
+  body: "Welcome to my first technical post...",
+  authorId: ObjectId('69a5f7732bb8a39e61711861'),
+  tags: ["MongoDB", "Backend", "Architecture"],
+  comments: []
+})
+
+
+db.posts.find()
+
+// output
+
+// {
+//   _id: ObjectId('69a5f8a62bb8a39e61711862'),
+//   title: 'Getting Started With DevBlog',
+//   body: 'Welcome to my first technical post...',
+//   authorId: ObjectId('69a5f7732bb8a39e61711861'),
+//   tags: [
+//     'MongoDB',
+//     'Backend',
+//     'Architecture'
+//   ],
+//   comments: []
+// }
