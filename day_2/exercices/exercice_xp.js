f34934d92fa14da39610ab3514335054fe19fba9
walid-habibi-2004 <habibiwalid553@gmail.com>
2026-03-04 21:13:36 +0000

    Resolve merge conflicts

---
+++ day_2/exercices/exercice_xp.js
@@ -0,0 +1,55 @@
+// ================> exercice 1
+db.createCollection("users")
+
+
+db.users.insertOne({
+  name: "Yassmine",
+  email: "yasmine@tech.ma",
+  address: {
+    city: "Casablanca",
+    country: "Morocco",
+    postalCode: "20000"
+  }
+})
+
+
+db.users.find()
+
+// output 
+
+// {
+//   _id: ObjectId('69a5eee02bb8a39e6171185f'),
+//   name: 'Yassmine',
+//   email: 'yasmine@tech.ma',
+//   address: {
+//     city: 'Casablanca',
+//     country: 'Morocco',
+//     postalCode: '20000'
+//   }
+// }
+
+
+// ================> exercice 2
+
+db.createCollection("orders")
+
+db.orders.insertOne({
+  orderNumber: "ORD-99X",
+  total: 1500,
+  currency: "MAD",
+  status: "Processing",
+  userId: ObjectId("69a5eee02bb8a39e6171185f")
+})
+
+db.orders.find()
+
+// output
+
+// {
+//   _id: ObjectId('69a5f2492bb8a39e61711860'),
+//   orderNumber: 'ORD-99X',
+//   total: 1500,
+//   currency: 'MAD',
+//   status: 'Processing',
+//   userId: ObjectId('69a5eee02bb8a39e6171185f')
+// }
